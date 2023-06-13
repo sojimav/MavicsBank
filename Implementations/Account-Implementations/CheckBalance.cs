d2722a0c96637d7831b4dfe66d0c378477ff4ef9
Victor Ajibade <adesojimav@gmail.com>
2023-06-13 14:44:39 +0100

    added the checkBalance class

---
+++ Implementations/Account-Implementations/CheckBalance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MavicsBank.Models.Account_Model;
+using MavicsBank.Models.Customer_Model;
+
+namespace MavicsBank.Implementations.Account_Implementations
+{
+    internal class CheckBalance
+    {
+        public void CheckMyBalance(Customer loggerdInCustomer)
+        {
+            List<Account> accounts = Acc_Helper.ReadFromAccountFile("Accounts.txt");
+
+        }
+    }
+}
